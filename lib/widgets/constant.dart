45b107a374c4ee0d448df8ed0c904df7914dc117
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-23 15:48:02 +0100

    Updating Project Constant

---
+++ lib/widgets/constant.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0XFFFF7643);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+    colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight);
+
+const kSecondaryColor = Color(0xFF979797);
+
+const kTextColor = Color(0xFF757575);
+
+const kAnimationDuration = Duration(milliseconds: 200);
