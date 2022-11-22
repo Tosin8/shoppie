bb92011933534c695fc357ba141e538a0b1c6b8e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-22 14:48:44 +0100

    Update constant with form error

--- lib/widgets/constant.dart
+++ lib/widgets/constant.dart
@@ -15,3 +15,13 @@ const kAnimationDuration = Duration(milliseconds: 200);
 
 const greyColor = Colors.grey;
 const textColor = Colors.black;
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r'^[a-zA-Z0-9,]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+
+const String kEmailNullError = 'Please Enter your email';
+const String kInvalidEmailError = 'Please Enter Valid Email';
+const String kPassNullError = 'Please Enter your password';
+const String kShortPassError = 'Password is too short';
+const String kMatchPassError = 'Passwords don\'t match';
