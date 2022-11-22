635727ac811d7cc33871a3dd609c6cb3b895e4ae
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-22 19:43:09 +0100

    Update body.dart
    With the SignForm StatefulWidget

--- lib/screens/forms/sign_in/components/body.dart
+++ lib/screens/forms/sign_in/components/body.dart
@@ -12,17 +12,35 @@ class _Body_LoginAccountState extends State<Body_LoginAccount> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Column(
-      children: [
-        Text(
-          'Welcome Back',
-          style: headingStyle,
-        ),
-        const Text(
-          'Sign in with your email and password \n or continue with your social media account',
-          textAlign: TextAlign.center,
-        ),
-      ],
+        child: SizedBox(
+      width: double.infinity,
+      child: Column(
+        children: [
+          Text(
+            'Welcome Back',
+            style: headingStyle,
+          ),
+          const Text(
+            'Sign in with your email and password \n or continue with your social media account',
+            textAlign: TextAlign.center,
+          ),
+          const SignForm(),
+        ],
+      ),
     ));
+  }
+}
+
+class SignForm extends StatefulWidget {
+  const SignForm({super.key});
+
+  @override
+  State<SignForm> createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
