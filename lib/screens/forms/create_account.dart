882938e4125f66f84cf99dfed39929d5348a05e6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-22 14:05:15 +0100

    Update create_account.dart

---
+++ lib/screens/forms/create_account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'; 
+
+class CreateAccount extends StatefulWidget {
+  const CreateAccount({super.key});
+
+  @override
+  State<CreateAccount> createState() => _CreateAccountState();
+}
+
+class _CreateAccountState extends State<CreateAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
