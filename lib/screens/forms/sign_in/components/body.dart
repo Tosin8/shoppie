18c9f1e6e62c4157ad089be63e86ff69a4dd7730
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-22 19:30:38 +0100

    Updating sign in
    With the body component

---
+++ lib/screens/forms/sign_in/components/body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Body_LoginAccount extends StatefulWidget {
+  const Body_LoginAccount({super.key});
+
+  @override
+  State<Body_LoginAccount> createState() => _Body_LoginAccountState();
+}
+
+class _Body_LoginAccountState extends State<Body_LoginAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -8,22 +8,30 @@ ThemeData theme() {
     primarySwatch: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     fontFamily: 'Muli',
-    appBarTheme: AppBarTheme(
-      color: Colors.white,
-      elevation: 0,
-      iconTheme: const IconThemeData(color: Colors.black),
-      systemOverlayStyle: SystemUiOverlayStyle.light,
-      toolbarTextStyle: const TextTheme(
-        headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
-      ).bodyText2,
-      titleTextStyle: const TextTheme(
-        headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
-      ).headline6,
-    ),
-    textTheme: const TextTheme(
-      bodyText1: TextStyle(color: kTextColor),
-      bodyText2: TextStyle(color: kTextColor),
-    ),
+    appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
     visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    bodyText1: TextStyle(color: kTextColor),
+    bodyText2: TextStyle(color: kTextColor),
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+    iconTheme: const IconThemeData(color: Colors.black),
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    toolbarTextStyle: const TextTheme(
+      headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
+    ).bodyText2,
+    titleTextStyle: const TextTheme(
+      headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
+    ).headline6,
   );
 }
