3f6982e17721c697a31d1cdcfeabc5f1e3174387
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-22 17:47:41 +0100

    Updating folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:shoppie/widgets/constant.dart';
+import 'package:shoppie/widgets/theme.dart';
 
 import 'screens/homepage.dart';
 
@@ -20,29 +19,4 @@ class MyApp extends StatelessWidget {
         theme: theme(),
         home: const HomePage());
   }
-}
-
-ThemeData theme() {
-  return ThemeData(
-    primarySwatch: Colors.blue,
-    scaffoldBackgroundColor: Colors.white,
-    fontFamily: 'Muli',
-    appBarTheme: AppBarTheme(
-      color: Colors.white,
-      elevation: 0,
-      iconTheme: const IconThemeData(color: Colors.black),
-      systemOverlayStyle: SystemUiOverlayStyle.light,
-      toolbarTextStyle: const TextTheme(
-        headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
-      ).bodyText2,
-      titleTextStyle: const TextTheme(
-        headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
-      ).headline6,
-    ),
-    textTheme: const TextTheme(
-      bodyText1: TextStyle(color: kTextColor),
-      bodyText2: TextStyle(color: kTextColor),
-    ),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-  );
 }
--- lib/widgets/theme.dart
+++ lib/widgets/theme.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'constant.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'Muli',
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      elevation: 0,
+      iconTheme: const IconThemeData(color: Colors.black),
+      systemOverlayStyle: SystemUiOverlayStyle.light,
+      toolbarTextStyle: const TextTheme(
+        headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
+      ).bodyText2,
+      titleTextStyle: const TextTheme(
+        headline6: TextStyle(color: Color(0xFFBDBDBD), fontSize: 18),
+      ).headline6,
+    ),
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(color: kTextColor),
+      bodyText2: TextStyle(color: kTextColor),
+    ),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
