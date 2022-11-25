acb48d48fe183fff3f0fff5b7ccc983e5721d96c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-25 21:54:10 +0100

    Update Social Card with Gesture Detector

--- lib/screens/forms/sign_in/components/social_card.dart
+++ lib/screens/forms/sign_in/components/social_card.dart
@@ -5,19 +5,26 @@ import '../../../../widgets/size_config.dart';
 class socialCard extends StatelessWidget {
   const socialCard({
     Key? key,
+    this.icon,
+    this.press,
   }) : super(key: key);
+
+  final String icon;
+  final Function press;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(getProportionateScreenWidth(12)),
-      height: getProportionateScreenHeight(40),
-      width: getProportionateScreenWidth(40),
-      decoration: const BoxDecoration(
-        color: Color(0xfff5f6f9),
-        shape: BoxShape.circle,
-      ),
-      child: Image.asset('assets/icons/fb_icon.png'),
-    );
+    return GestureDetector(
+        onTap: press,
+        child: Container(
+          padding: EdgeInsets.all(getProportionateScreenWidth(12)),
+          height: getProportionateScreenHeight(40),
+          width: getProportionateScreenWidth(40),
+          decoration: const BoxDecoration(
+            color: Color(0xfff5f6f9),
+            shape: BoxShape.circle,
+          ),
+          child: Image.asset(icon),
+        ));
   }
 }
