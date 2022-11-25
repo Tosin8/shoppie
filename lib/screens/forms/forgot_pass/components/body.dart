af042e348a0945040764517f7312be9e4a346d06
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-25 23:33:35 +0100

    Updating Body with SingleScrollWidget

--- lib/screens/forms/forgot_pass/components/body.dart
+++ lib/screens/forms/forgot_pass/components/body.dart
@@ -10,23 +10,25 @@ class Body_ForgotPass extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        children: [
-          SizedBox(height: SizeConfig.screenHeight * 0.04),
-          Text(
-            'Forgot Password',
-            style: TextStyle(
-              fontSize: getProportionateScreenWidth(28),
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: SizeConfig.screenHeight * 0.04),
+            Text(
+              'Forgot Password',
+              style: TextStyle(
+                fontSize: getProportionateScreenWidth(28),
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const Text(
-            'Please enter your email and we will send \n you a link to return to your account',
-            textAlign: TextAlign.center,
-          ),
-          const ForgotPassForm(),
-        ],
+            const Text(
+              'Please enter your email and we will send \n you a link to return to your account',
+              textAlign: TextAlign.center,
+            ),
+            const ForgotPassForm(),
+          ],
+        ),
       ),
     );
   }
