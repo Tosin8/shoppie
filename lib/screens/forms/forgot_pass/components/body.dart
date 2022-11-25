58658bb8435f169ab5bb891a2a4109ed69612780
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-25 22:59:57 +0100

    Update body.dart

--- lib/screens/forms/forgot_pass/components/body.dart
+++ lib/screens/forms/forgot_pass/components/body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shoppie/widgets/size_config.dart';
 
 class Body_ForgotPass extends StatelessWidget {
   const Body_ForgotPass({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Text(
+          'Forgot Password',
+          style: TextStyle(
+              fontSize: getProportionateScreenWidth(28), color: Colors.black),
+        )
+      ],
+    );
   }
 }
