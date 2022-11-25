1cd8f7ec02eae3815e2c749e6af2317b89b32c4f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-25 22:40:58 +0100

    Updating Route with the forgotpassword class

--- lib/widgets/routes.dart
+++ lib/widgets/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shoppie/screens/forms/forgot_pass/forgot_pass.dart';
 
 import '../screens/forms/create_account/create_account.dart';
 import '../screens/forms/sign_in/login.dart';
@@ -9,4 +10,5 @@ import '../screens/forms/sign_in/login.dart';
 final Map<String, WidgetBuilder> routes = {
   CreateAccount.routeName: (context) => const CreateAccount(),
   LoginAccount.routeName: (context) => const LoginAccount(),
+  ForgotPassword.routeName: (context) => const ForgotPassword(),
 };
