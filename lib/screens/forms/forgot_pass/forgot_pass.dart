02e8c1623b8f4ba7d2d228a1f26c3c1cd858df47
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-25 22:36:41 +0100

    Create Forgot Password

---
+++ lib/screens/forms/forgot_pass/forgot_pass.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  static String routeName = '/forgot_password';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
