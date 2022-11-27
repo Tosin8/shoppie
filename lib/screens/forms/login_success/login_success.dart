6be9685efdaf6fae4bdb1a4ffaf1e0594c7d876f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-27 19:48:04 +0100

    Updating with stateful widget

---
+++ lib/screens/forms/login_success/login_success.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class login_success extends StatefulWidget {
+  const login_success({super.key});
+
+  @override
+  State<login_success> createState() => _login_successState();
+}
+
+class _login_successState extends State<login_success> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
